47336066e6b77600fd2147ec1b78a90d2319899e
islamhack01 <iaubakirov503@gmail.com>
2022-10-27 13:20:30 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,66 @@
+let b1 = document.querySelector('#b1')
+let lift = document.querySelector('#lift')
+b1.addEventListener('click', ()=>{
+    lift.style.top = '790px'
+})
+let b2 = document.querySelector('#b2')
+b2.addEventListener('click', ()=>{
+    lift.style.top = '740px'
+})
+let b3 = document.querySelector('#b3')
+b3.addEventListener('click', ()=>{
+    lift.style.top = '690px'
+})
+let b4 = document.querySelector('#b4')
+b4.addEventListener('click', ()=>{
+    lift.style.top = '640px'
+})
+let b5 = document.querySelector('#b5')
+b5.addEventListener('click', ()=>{
+    lift.style.top = '590px'
+})
+let b6 = document.querySelector('#b6')
+b6.addEventListener('click', ()=>{
+    lift.style.top = '540px'
+})
+let b7 = document.querySelector('#b7')
+b7.addEventListener('click', ()=>{
+    lift.style.top = '490px'
+})
+let b8 = document.querySelector('#b8')
+b8.addEventListener('click', ()=>{
+    lift.style.top = '440px'
+})
+let b9 = document.querySelector('#b9')
+b9.addEventListener('click', ()=>{
+    lift.style.top = '390px'
+})
+let b10 = document.querySelector('#b10')
+b10.addEventListener('click', ()=>{
+    lift.style.top = '340px'
+})
+let b11 = document.querySelector('#b11')
+b11.addEventListener('click', ()=>{
+    lift.style.top = '290px'
+})
+let b12 = document.querySelector('#b12')
+b12.addEventListener('click', ()=>{
+    lift.style.top = '240px'
+})
+let b13 = document.querySelector('#b13')
+b13.addEventListener('click', ()=>{
+    lift.style.top = '190px'
+})
+let b14 = document.querySelector('#b14')
+b14.addEventListener('click', ()=>{
+    lift.style.top = '140px'
+})
+let b15 = document.querySelector('#b15')
+b15.addEventListener('click', ()=>{
+    lift.style.top = '90px'
+})
+let b16 = document.querySelector('#b16')
+b16.addEventListener('click', ()=>{
+    lift.style.top = '40px'
+})
+
